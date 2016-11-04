d39b7127ea6a940c469602e75e92f6eb50162457
Flavio <flavio.fatesg@yahoo.com.br>
2016-11-03 22:43:36 -0200

    Player

---
+++ Player/frmMain.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Player
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxStreamingUrl = new System.Windows.Forms.TextBox();
+            this.progressBarBuffer = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelBuffered = new System.Windows.Forms.Label();
+            this.buttonPause = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.volumeSlider1 = new NAudio.Gui.VolumeSlider();
+            this.SuspendLayout();
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(32, 129);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlay.TabIndex = 0;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Stream URL";
+            // 
+            // textBoxStreamingUrl
+            // 
+            this.textBoxStreamingUrl.Location = new System.Drawing.Point(100, 41);
+            this.textBoxStreamingUrl.Name = "textBoxStreamingUrl";
+            this.textBoxStreamingUrl.Size = new System.Drawing.Size(259, 20);
+            this.textBoxStreamingUrl.TabIndex = 2;
+            this.textBoxStreamingUrl.Text = "http://radio.reaper.fm/stream/";
+            // 
+            // progressBarBuffer
+            // 
+            this.progressBarBuffer.Location = new System.Drawing.Point(100, 72);
+            this.progressBarBuffer.Name = "progressBarBuffer";
+            this.progressBarBuffer.Size = new System.Drawing.Size(259, 23);
+            this.progressBarBuffer.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Buffered";
+            // 
+            // labelBuffered
+            // 
+            this.labelBuffered.AutoSize = true;
+            this.labelBuffered.Location = new System.Drawing.Point(370, 82);
+            this.labelBuffered.Name = "labelBuffered";
+            this.labelBuffered.Size = new System.Drawing.Size(27, 13);
+            this.labelBuffered.TabIndex = 5;
+            this.labelBuffered.Text = "0.3s";
+            // 
+            // buttonPause
+            // 
+            this.buttonPause.Location = new System.Drawing.Point(113, 129);
+            this.buttonPause.Name = "buttonPause";
+            this.buttonPause.Size = new System.Drawing.Size(75, 23);
+            this.buttonPause.TabIndex = 6;
+            this.buttonPause.Text = "Pause";
+            this.buttonPause.UseVisualStyleBackColor = true;
+            this.buttonPause.Click += new System.EventHandler(this.buttonPause_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(194, 129);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 7;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // volumeSlider1
+            // 
+            this.volumeSlider1.Location = new System.Drawing.Point(210, 129);
+            this.volumeSlider1.Name = "volumeSlider1";
+            this.volumeSlider1.Size = new System.Drawing.Size(109, 19);
+            this.volumeSlider1.TabIndex = 9;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 225);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonPause);
+            this.Controls.Add(this.labelBuffered);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBarBuffer);
+            this.Controls.Add(this.textBoxStreamingUrl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonPlay);
+            this.Name = "frmMain";
+            this.Text = "Flavio Audio Stream Player";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxStreamingUrl;
+        private System.Windows.Forms.ProgressBar progressBarBuffer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelBuffered;
+        private System.Windows.Forms.Button buttonPause;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Timer timer1;
+        private NAudio.Gui.VolumeSlider volumeSlider1;
+    }
+}
+
